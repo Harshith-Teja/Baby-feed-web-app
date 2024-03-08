c1d1af88fe543e491efc809c0ff84c2b1a268f16
Harshith Teja <harshithtejadontoju@gmail.com>
2024-03-08 17:29:22 +0530

    added jwts to the authController and verifyJWT to protect routes

--- back-end/controllers/authController.js
+++ back-end/controllers/authController.js
@@ -5,6 +5,11 @@ const usersDB = {
 
 const bcrypt = require('bcrypt');
 
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const fsPromises = require('fs').promises;
+const path = require('path');
+
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
     if(!user || !pwd) 
@@ -20,8 +25,33 @@ const handleLogin = async (req, res) => {
         const match = await bcrypt.compare(pwd, foundUser.password);
         
         if(match) {
-            //create JWTs
-            res.json({"success": `User ${user} is logged in!`});
+            //creating JWTs
+            const accessToken = jwt.sign(
+                {"username" : foundUser.username},
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: '30s'} //normally can keep it to 5-15min
+            );
+
+            const refreshToken = jwt.sign(
+                {"username" : foundUser.username},
+                process.env.REFRESH_TOKEN_SECRET,
+                { expiresIn: '1d'}
+            );
+
+            //saving the refresh token(with the curr user) to the db
+            const otherUsers = usersDB.users.filter(person => person.username !== foundUser.username);
+            const currUser = {...foundUser, refreshToken};
+            usersDB.setUsers([...otherUsers, currUser]);
+
+            //writing the changes to the users.json file
+            await fsPromises.writeFile(
+                path.join(__dirname, '..', 'model', 'users.json'),
+                JSON.stringify(usersDB.users)
+            );
+            
+            //sending aT & rT to the client(browser)
+            res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000}); //httpOnly is set to true(i.e, it is not sent in js and it can't be hacked and accessed)
+            res.json({ accessToken });
         } else {
             res.sendStatus(401);
         }
