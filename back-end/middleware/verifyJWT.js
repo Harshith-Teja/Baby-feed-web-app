5351fa06ebbc8e705d7ead8ce07e49dab3ef5e1a
Harshith Teja <harshithtejadontoju@gmail.com>
2024-03-09 14:29:08 +0530

    fixed a bug in verifyJWT.js

--- back-end/middleware/verifyJWT.js
+++ back-end/middleware/verifyJWT.js
@@ -2,10 +2,10 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const verifyJWT = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    if(!authHeader)
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+    if(!authHeader?.startsWith('Bearer '))
         return res.sendStatus(401);
-    console.log(authHeader); //Bearer token
+  //  console.log(authHeader); //Bearer token
 
     const token = authHeader.split(' ')[1]; //extracting token from authHeader
     jwt.verify(
