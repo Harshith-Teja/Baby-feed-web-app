f42e92bacebf65d9d0be23a40a1e4681c1e64e09
Harshith Teja <harshithtejadontoju@gmail.com>
2024-03-09 14:41:41 +0530

    fixed the issue in allowedOrigins.js, and successfully  tested the code, now requests can be made from the /register route to the backend

--- back-end/config/allowedOrigins.js
+++ back-end/config/allowedOrigins.js
@@ -1,5 +1,7 @@
 const allowedOrigins = [
     'https://www.yoursite.com',
     'http://127.0.0.1:5173',
-    'http://localhost:3500',
+    'http://localhost:3500'
 ];
+
+module.exports = allowedOrigins;
