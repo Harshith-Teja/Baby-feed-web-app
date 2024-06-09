52d8ea8f07c8c64f9ed9ef70d6eac29dfc9032e2
Harshith Teja <harshithtejadontoju@gmail.com>
2024-06-09 17:19:34 +0530

    Ready to deploy

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons" 
+import { useNavigate } from "react-router-dom";
+import axios from '../api/axios'
 
 export const Header = () => {
+  const navigate = useNavigate()
+
+  const handleLogout = async () => {
+      await axios.get('/logout')
+      navigate('/login')
+  }
+
   return (
-    <header className="bg-gradient-to-r from-indigo-400 to-cyan-400 shadow-2xl shadow-indigo-500/50 text-slate-100 text-4xl text-center p-4">
+    <header className="bg-gradient-to-r from-indigo-400 to-cyan-400 shadow-2xl shadow-indigo-500/50 text-slate-100 text-4xl text-center p-4 relative">
       Baby Feed Web Application
+
+      <span className="absolute inset-y-0 right-0 p-4">
+      <FontAwesomeIcon icon={faRightFromBracket} onClick={handleLogout}/>
+      </span>
     </header>
   );
 };
